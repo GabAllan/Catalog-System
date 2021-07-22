af1f37eb80c83e940aa3050d504a5a0a4f0b39e0
bupod <33789508+GabAllan@users.noreply.github.com>
2021-07-22 02:21:25 -0400

    Nightly Progress for July 21
    I've decided to abandon the Jtree for now. I don't think I'll be able to get it to work in time, and there aren't very many good tutorials out on it except for the Javadocs. Trying to adapt it to my use case will be hard at my skill level, and I think a drop-down menu and list will suffice (and probably look cleaner).

--- GabesPartsLibrary/src/AdminScreen.java
+++ GabesPartsLibrary/src/AdminScreen.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class AdminScreen extends JFrame{
+public class AdminScreen extends JFrame  {
     private JPanel assemblyDiagram;
     private JPanel adminScreen;
-    private JButton addEditButton;
-    private JButton delete;
-    private JButton edit;
-    private JList list1;
-    private JLabel engineLabel;
+    private JButton insertButton;
+    private JButton deleteButton;
+    private JButton editButton;
+    private JList partsList;
+    private JComboBox engineSelector;
 
 
     public AdminScreen(String title) {
@@ -20,16 +20,36 @@ public class AdminScreen extends JFrame{
         this.pack();
 
 
-
         // The following should open a new window that will allow the
         // addition of new engines and sub-assemblies as well
         // as editing current ones
-        addEditButton.addActionListener(new ActionListener() {
+        insertButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
+
+        // The following should first prompt an "Are you sure?" dialog
+        // followed by deleting the currently selected item in the Jtree
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // The following should open a dialog window similar
+        // to the insert button, but with pre-populated fields
+        // based on the selected object in the jTree. It will
+        // allow the user to edit the existing data
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
     }
 
 }
